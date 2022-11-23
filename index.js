f5c83d5cf8d10df5302b46f11b40f87d0aa0bf72
Atharva Pardeshi <atharva.exe@gmail.com>
2022-11-23 13:20:04 +0530

    feat: use readline to prompt the user and then read the output

--- index.js
+++ index.js
@@ -98,3 +98,19 @@ const db = new Low(adapter);
 
 // Set some defaults
 db.data ||= { todos: [] };
+
+// Prompt user to input data
+const prompt = (question) => {
+  const r = rl.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false,
+  });
+
+  return new Promise((resolve, error) => {
+    r.question(question, (answer) => {
+      r.close();
+      resolve(answer);
+    });
+  });
+};
